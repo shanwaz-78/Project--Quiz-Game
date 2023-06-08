903c5c2bcc3d559038590cc7974a55674e3d074a
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-06-08 16:00:57 +0530

    Made Random Color Generator

--- Random Color Generator/js/script.js
+++ Random Color Generator/js/script.js
@@ -1,9 +1,8 @@
 const h3Elem = document.getElementById("color-code");
 let randomCode;
 function generateRandomColor() {
-  const randomNumber = parseInt(Math.random() * 16777215);
-  randomCode = "#" + randomNumber.toString(16);
-  document.body.style.backgroundColor = randomCode;
+  const randomNumber = parseInt(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomCode;
   h3Elem.innerText = randomCode;
 }
 
