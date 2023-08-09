92861f6878f1340e8174fecbf3d0ed0d540a3d18
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-08-10 01:33:58 +0530

    Made chnages in routes

--- portfolio website/src/index.js
+++ portfolio website/src/index.js
@@ -1,7 +1,6 @@
 const connection = require("../database/connection");
 const express = require("express");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 8001;
 const path = require("path");
 const app = express();
@@ -12,6 +11,7 @@ const dashBoardPath = path.join(__dirname, "../public");
 const loginPagePath = path.join(__dirname, "../public/html/login.html");
 const registerPagePath = path.join(__dirname, "../public/html/register.html");
 const errPage = path.join(__dirname, "../public/html/404.html");
+const update = path.join(__dirname, "../public/html/update.html");
 // end static paths
 
 // built-in middleware
@@ -37,46 +37,21 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { name, password } = req.body;
-    const rows = await connection.query(
-      "SELECT * FROM user_info WHERE name = ?",
-      name
+    await connection.query(
+      "INSERT INTO login(name, password) VALUES(?, ?)",
+      [name, password]
     );
-    if (rows.length === 0) {
-      return res.redirect("/register");
-    }
-
-    const hashedPassword = rows[0].password;
-    // Compare the provided password with the encrypted password
-    const match = await bcrypt.compare(password, hashedPassword);
-
-    if (match) {
-      return res.redirect("/");
-    } else {
-      return res.redirect("/register");
-    }
+    res.redirect('/')
   } catch (error) {
     res.status(500).send("Internal Server Error");
   }
 });
 
-async function generateToken() {
-  const token = await jwt.sign(
-    { id: 204939223049492 },
-    "kkkaslleleljgjalml2l3lsdkkeopaldalc"
-  );
-  console.log(token);
-  const userVerify = await jwt.verify(
-    token,
-    "kkkaslleleljgjalml2l3lsdkkeopaldalc"
-  );
-  console.log(userVerify);
-}
-generateToken();
-
 // start routes
 app.get("/", (req, res) => res.sendFile(dashBoardPath));
 app.get("/login", (req, res) => res.sendFile(loginPagePath));
 app.get("/register", (req, res) => res.sendFile(registerPagePath));
+app.get("/users", (req, res) => res.sendFile(update));
 app.get("*", (req, res) => res.sendFile(errPage));
 // end routes
 
