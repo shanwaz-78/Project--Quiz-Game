68f9f54628585b7ab01946a12a9981a83fd8195e
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-08-21 19:33:28 +0530

    Made Pagination page

---
+++ Pagination/js/script.js
@@ -0,0 +1,36 @@
+const tableBody = document.querySelector("tbody");
+const itemsPerPage = 5;
+let currentPage = 1;
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error at fetchData Function`);
+    }
+    const data = await response.json();
+    showData(data.users);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+tableBody.innerHTML = ""; // Clear existing rows
+function showData(data) {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  for (let i = startIndex; i < endIndex && i < data.length; i++) {
+    const userData = data[i];
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${userData.id}</td>
+      <td>${userData.firstName}</td>
+      <td>${userData.email}</td>
+      <td>${userData.phone}</td>
+    `;
+    tableBody.appendChild(row);
+  }
+}
+
+fetchData("https://dummyjson.com/users");
