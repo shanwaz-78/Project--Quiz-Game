fccbf078f2c2afa628565c2694ba3a545f687a56
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-10-19 17:47:49 +0530

    Made Custom Sentence Format Changer

---
+++ custom word changer/js/index.js
@@ -0,0 +1,60 @@
+const inputField = document.getElementById("userInput");
+const formatBtns = document.querySelectorAll(".btn");
+const copyBtn = document.getElementById("copy");
+const outputField = document.getElementById("output");
+
+function handleUserInput() {
+  outputField.textContent = inputField.value;
+}
+
+function handleBtnAnimation(prefix = "animate__") {
+  formatBtns.forEach((btn) =>
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      btn.classList.add(
+        `${prefix}animated`,
+        `${prefix}rubberBand`,
+        `${prefix}faster`
+      );
+      setTimeout(() => {
+        btn.classList.remove(`${prefix}rubberBand`);
+      }, 800);
+    })
+  );
+}
+
+function handleBtnOptions() {
+  formatBtns.forEach((btn) =>
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      const clientGiveValue = inputField.value;
+      switch (btn.textContent) {
+        case "ABC":
+          outputField.textContent = clientGiveValue.toUpperCase();
+          break;
+        case "abc":
+          outputField.textContent = clientGiveValue.toLowerCase();
+          break;
+        case "Abc":
+          outputField.textContent =
+            clientGiveValue.charAt(0).toUpperCase() + clientGiveValue.slice(1);
+          break;
+        case "Bold":
+          outputField.innerHTML = `<strong>${clientGiveValue}</strong>`;
+          break;
+        case "Italic":
+          outputField.innerHTML = `<em>${clientGiveValue}</em>`;
+          break;
+        case "Underline":
+          outputField.innerHTML = `<u>${clientGiveValue}</u>`;
+          break;
+        default:
+          throw new Error(`Select Right Option`);
+          break;
+      }
+    })
+  );
+}
+handleBtnAnimation();
+handleBtnOptions();
+inputField.addEventListener("input", handleUserInput);
